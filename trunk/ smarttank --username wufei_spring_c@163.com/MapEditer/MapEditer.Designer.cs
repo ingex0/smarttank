3a61a50ed5c3df5849289b9f5329ac7934dc49a1
wufei_spring_c@163.com <wufei_spring_c@163.com@24b464f8-a43f-0410-b117-8d365baceae5>
2007-12-07 02:05:21 +0000

    更新了场景物体编辑器。准备编写场景编辑器

--- trunk/ smarttank --username wufei_spring_c@163.com/MapEditer/MapEditer.Designer.cs
+++ trunk/ smarttank --username wufei_spring_c@163.com/MapEditer/MapEditer.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent ()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MapEditer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 12F );
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size( 941, 578 );
+            this.Name = "MapEditer";
             this.Text = "MapEditer";
+            this.ResumeLayout( false );
+
         }
 
         #endregion
